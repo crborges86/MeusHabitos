14222f00527623625f3a5c6a42ca2994719ac17f
Cristiano Borges <143454517+crborges86@users.noreply.github.com>
2026-01-07 06:03:16 -0300

    Initial Commit

---
+++ MeusHabitos/MeusHabitosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeusHabitosApp.swift
+//  MeusHabitos
+//
+//  Created by Cristiano Ricardo Borges on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MeusHabitosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
