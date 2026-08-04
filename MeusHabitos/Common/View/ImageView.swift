dde0c12704a9297949746d9a6162cad7683e6aed
Cristiano Ricardo Borges <cristianoborges86@gmail.com>
2026-08-04 06:35:18 -0300

    Criação de ImageView

---
+++ MeusHabitos/Common/View/ImageView.swift
@@ -0,0 +1,69 @@
+//
+//  ImageView.swift
+//  MeusHabitos
+//
+//  Created by Cristiano Ricardo Borges on 04/08/26.
+//
+
+import SwiftUI
+import Combine
+
+struct ImageView: View {
+    
+    @State private var image: UIImage = UIImage()
+    @ObservedObject var imageLoader = ImageLoader()
+    
+    let url: String
+    
+    var body: some View {
+        Image(uiImage: image)
+            .resizable()
+            .onReceive(imageLoader.didChange) { data in
+                self.image = UIImage(data: data) ?? UIImage()
+            }
+            .onAppear {
+                if image.cgImage == nil {
+                    imageLoader.load(url: url)
+                }
+            }
+    }
+}
+
+class ImageLoader: ObservableObject {
+    
+    var didChange = PassthroughSubject< Data, Never>()
+    
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+    
+    init() { }
+    
+    func load(url: String) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                
+                self.data = data
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView(url: "http://google.com")
+    }
+}
--- MeusHabitos/Habit/View/HabitCardView.swift
+++ MeusHabitos/Habit/View/HabitCardView.swift
@@ -31,8 +31,10 @@ struct HabitCardView: View {
             }, label: {
                 
                 HStack {
-                    Image(systemName: "pencil")
-                        .padding(.horizontal, 8)
+                    ImageView(url: viewModel.icon)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 32, height: 32)
+                        .clipped()
                     
                     Spacer()
                     
