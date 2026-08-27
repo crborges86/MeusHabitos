2a102b9a08c6ebb7ff7bce6a9f5f66fdeaece276
Cristiano Ricardo Borges <cristianoborges86@gmail.com>
2026-08-27 06:43:25 -0300

    Criado BoxChartView, ChartViewModel e modificado o ChartView

---
+++ MeusHabitos/Charts/View/BoxChartView.swift
@@ -0,0 +1,8 @@
+//
+//  BoxChartView.swift
+//  MeusHabitos
+//
+//  Created by Cristiano Ricardo Borges on 27/08/26.
+//
+
+import Foundation
---
+++ MeusHabitos/Charts/ViewModel/ChartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  C hartViewModel.swift
+//  MeusHabitos
+//
+//  Created by Cristiano Ricardo Borges on 27/08/26.
+//
+
+import Foundation
